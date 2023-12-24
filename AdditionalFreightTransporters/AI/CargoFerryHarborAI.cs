42b40a7ba5e76de7f99c35d27e19867f11690022
Tal <talrofeh@gmail.com>
2023-12-24 20:48:04 +0200

    fixes

--- AdditionalFreightTransporters/AI/CargoFerryHarborAI.cs
+++ AdditionalFreightTransporters/AI/CargoFerryHarborAI.cs
@@ -27,62 +27,47 @@ namespace AdditionalFreightTransporters.AI
         public override void InitializePrefab()
         {
             base.InitializePrefab();
-            float a = this.m_info.m_generatedInfo.m_max.z - 7f;
-            if (this.m_info.m_paths != null)
+            float a = m_info.m_generatedInfo.m_max.z - 7f;
+            if (m_info.m_paths != null)
             {
-                for (int index1 = 0; index1 < this.m_info.m_paths.Length; ++index1)
+                for (int index1 = 0; index1 < m_info.m_paths.Length; ++index1)
                 {
-                    if (this.m_info.m_paths[index1].m_netInfo != null &&
-                        this.m_info.m_paths[index1].m_netInfo.m_class.m_service == ItemClass.Service.Road &&
-                        this.m_info.m_paths[index1].m_nodes != null)
+                    if (m_info.m_paths[index1].m_netInfo != null && m_info.m_paths[index1].m_netInfo.m_class.m_service == ItemClass.Service.Road && m_info.m_paths[index1].m_nodes != null)
                     {
-                        for (int index2 = 0; index2 < this.m_info.m_paths[index1].m_nodes.Length; ++index2)
-                            a = Mathf.Min(a,
-                                -16f - this.m_info.m_paths[index1].m_netInfo.m_halfWidth -
-                                this.m_info.m_paths[index1].m_nodes[index2].z);
+                        for (int index2 = 0; index2 < m_info.m_paths[index1].m_nodes.Length; ++index2)
+                        {
+                            a = Mathf.Min(a, -16f - m_info.m_paths[index1].m_netInfo.m_halfWidth - m_info.m_paths[index1].m_nodes[index2].z);
+                        }   
                     }
                 }
             }
-
-            this.m_quayOffset = a;
+            m_quayOffset = a;
         }
 
-        public override ToolBase.ToolErrors CheckBuildPosition(
-            ushort relocateID,
-            ref Vector3 position,
-            ref float angle,
-            float waterHeight,
-            float elevation,
-            ref Segment3 connectionSegment,
-            out int productionRate,
-            out int constructionCost)
+        public override ToolBase.ToolErrors CheckBuildPosition(ushort relocateID, ref Vector3 position, ref float angle, float waterHeight, float elevation, ref Segment3 connectionSegment, out int productionRate, out int constructionCost)
         {
             ToolBase.ToolErrors toolErrors1 = ToolBase.ToolErrors.None;
-            Vector3 pos;
-            Vector3 dir;
-            bool isQuay;
-            if (this.m_info.m_placementMode == BuildingInfo.PlacementMode.Shoreline &&
-                BuildingTool.SnapToCanal(position, out pos, out dir, out isQuay, 40f, false))
+            if (m_info.m_placementMode == BuildingInfo.PlacementMode.Shoreline && BuildingTool.SnapToCanal(position, out Vector3 pos, out Vector3 dir, out bool isQuay, 40f, false))
             {
                 angle = Mathf.Atan2(dir.x, -dir.z);
-                pos += dir * this.m_quayOffset;
+                pos += dir * m_quayOffset;
                 position.x = pos.x;
                 position.z = pos.z;
                 if (!isQuay)
+                {
                     toolErrors1 |= ToolBase.ToolErrors.ShoreNotFound;
+                } 
             }
-
-            ToolBase.ToolErrors toolErrors2 = toolErrors1 | base.CheckBuildPosition(relocateID, ref position, ref angle,
-                waterHeight, elevation, ref connectionSegment, out productionRate, out constructionCost);
-
+            ToolBase.ToolErrors toolErrors2 = toolErrors1 | base.CheckBuildPosition(relocateID, ref position, ref angle, waterHeight, elevation, ref connectionSegment, out productionRate, out constructionCost);
             return toolErrors2;
         }
 
         public override bool GetWaterStructureCollisionRange(out float min, out float max)
         {
-            if (this.m_info.m_placementMode != BuildingInfo.PlacementMode.Shoreline)
+            if (m_info.m_placementMode != BuildingInfo.PlacementMode.Shoreline)
+            {
                 return base.GetWaterStructureCollisionRange(out min, out max);
-            min = 20f / Mathf.Max(22f, (float)this.m_info.m_cellLength * 8f);
+            }            min = 20f / Mathf.Max(22f, m_info.m_cellLength * 8f);
             max = 1f;
             return true;
         }
--- AdditionalFreightTransporters/AI/CargoTramDepotAI.cs
+++ AdditionalFreightTransporters/AI/CargoTramDepotAI.cs
@@ -25,24 +25,21 @@ namespace AdditionalFreightTransporters.AI
         public override void InitializePrefab()
         {
             base.InitializePrefab();
-            float a = this.m_info.m_generatedInfo.m_max.z - 7f;
-            if (this.m_info.m_paths != null)
+            float a = m_info.m_generatedInfo.m_max.z - 7f;
+            if (m_info.m_paths != null)
             {
-                for (int index1 = 0; index1 < this.m_info.m_paths.Length; ++index1)
+                for (int index1 = 0; index1 < m_info.m_paths.Length; ++index1)
                 {
-                    if (this.m_info.m_paths[index1].m_netInfo != null &&
-                        this.m_info.m_paths[index1].m_netInfo.m_class.m_service == ItemClass.Service.Road &&
-                        this.m_info.m_paths[index1].m_nodes != null)
+                    if (m_info.m_paths[index1].m_netInfo != null && m_info.m_paths[index1].m_netInfo.m_class.m_service == ItemClass.Service.Road && m_info.m_paths[index1].m_nodes != null)
                     {
-                        for (int index2 = 0; index2 < this.m_info.m_paths[index1].m_nodes.Length; ++index2)
-                            a = Mathf.Min(a,
-                                -16f - this.m_info.m_paths[index1].m_netInfo.m_halfWidth -
-                                this.m_info.m_paths[index1].m_nodes[index2].z);
+                        for (int index2 = 0; index2 < m_info.m_paths[index1].m_nodes.Length; ++index2)
+                        {
+                            a = Mathf.Min(a, -16f - m_info.m_paths[index1].m_netInfo.m_halfWidth - m_info.m_paths[index1].m_nodes[index2].z);
+                        }
                     }
                 }
             }
-
-            this.m_quayOffset = a;
+            m_quayOffset = a;
         }
 
         public override bool RequireRoadAccess() => true;
