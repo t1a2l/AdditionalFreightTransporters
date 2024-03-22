a149ba6d3567e12c0f81467eb7f14ec3e29ab6a3
Tal <talrofeh@gmail.com>
2024-03-22 18:43:46 +0200

    update cargo heli to 3 cargo trucks capacity

---
+++ AdditionalFreightTransporters/HarmonyPatches/InitializePrefabVehiclePatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AdditionalFreightTransporters.AI;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AdditionalFreightTransporters.HarmonyPatches
+{
+    [HarmonyPatch(typeof(VehicleInfo))]
+    internal static class InitializePrefabVehiclePatch
+    {
+        [HarmonyPatch(typeof(VehicleInfo), "InitializePrefab")]
+        [HarmonyPrefix]
+        public static bool InitializePrefab(VehicleInfo __instance)
+        {
+            try
+            {
+                var component = __instance.GetComponent<PrefabAI>();
+                if (__instance.m_class.m_service == ItemClass.Service.PublicTransport && __instance.m_class.m_subService == ItemClass.SubService.PublicTransportPlane 
+                    && __instance.m_class.m_level == ItemClass.Level.Level5 && __instance.m_vehicleType == VehicleInfo.VehicleType.Helicopter && component != null && component is CargoHelicopterAI cargoHelicopterAI)
+                {
+                    cargoHelicopterAI.m_cargoCapacity = 3;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+
+            return true;
+        }
+    }
+}
